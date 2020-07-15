e7e7dbbadcc0ceb711f760409d94ff3faed59d08
maiali13 <maryamfawaz@gmail.com>
2020-07-15 15:40:29 -0700

    part 1

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,6 @@
+import math, random
+
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -38,15 +41,29 @@ class SocialGraph:
 
         The number of users must be greater than the average number of friendships.
         """
-        # Reset graph
+       # Reset graph
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
-
+​
         # Add users
-
+        for i in range(num_users):
+            self.add_user(f"User {i}")
+​
         # Create friendships
+        possible_friendships = []
+​
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+​
+        # Shuffle the possible friendships
+        random.shuffle(possible_friendships)
+​
+        # Add friendships
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
